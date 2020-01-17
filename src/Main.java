e683f4398d9efe5f731c5b068a7a12011d1efd04
AdriMer <adrien.mercelat@orange.fr>
2020-01-17 15:14:58 +0100

    New Class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     Client cl1 = new Client("Mercelat","Adrien","adrien.mercelat@orange.fr");
     Client cl2 = new Client("Caillat","Ronan","ronan.caillat@gmail.com");
-    Client cl3 = new Client("Wawer","Quentin","quantin.wawer@gmail.com");
+    Client cl3 = new Client("Wawer","Quentin","quentin.wawer@gmail.com");
 
     Chambre ch1 = new Chambre(100, 4, 25, Categorie.Eco);
     Chambre ch2 = new Chambre(200, 4, 45, Categorie.Confort);
