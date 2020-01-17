947e74ad173b5865a279d79b1bf7e68f7dbc8493
AdriMer <adrien.mercelat@orange.fr>
2020-01-17 15:53:44 +0100

    New Class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
-public class Main {
-    Client cl1 = new Client("Mercelat","Adrien","adrien.mercelat@orange.fr");
-    Client cl2 = new Client("Caillat","Ronan","ronan.caillat@gmail.com");
-    Client cl3 = new Client("Wawer","Quentin","quentin.wawer@gmail.com");
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-    Chambre ch1 = new Chambre(100, 4, 25, Categorie.Eco);
-    Chambre ch2 = new Chambre(200, 4, 45, Categorie.Confort);
-    Chambre ch3 = new Chambre(300, 4, 65, Categorie.ConfortPlus);
+public class Main {
+    public static void main() throws ParseException {
+        Client cl1 = new Client("Mercelat","Adrien","adrien.mercelat@orange.fr");
+        Client cl2 = new Client("Caillat","Ronan","ronan.caillat@gmail.com");
+        Client cl3 = new Client("Wawer","Quentin","quentin.wawer@gmail.com");
+
+        Chambre ch1 = new Chambre(100, 4, 25, Categorie.Eco);
+        Chambre ch2 = new Chambre(200, 4, 45, Categorie.Confort);
+        Chambre ch3 = new Chambre(300, 4, 65, Categorie.ConfortPlus);
+
+        cl1.reserver(new SimpleDateFormat("dd/MM/yyyy").parse("17/01/2020"),
+                new SimpleDateFormat("dd/MM/yyyy").parse("27/01/2020"),
+                3, Categorie.Confort);
+
+        cl2.reserver(new SimpleDateFormat("dd/MM/yyyy").parse("20/01/2020"),
+                new SimpleDateFormat("dd/MM/yyyy").parse("27/01/2020"),
+                3, Categorie.Confort);
+
+        cl3.reserver(new SimpleDateFormat("dd/MM/yyyy").parse("22/01/2020"),
+                new SimpleDateFormat("dd/MM/yyyy").parse("30/01/2020"),
+                3, Categorie.Confort);
+    }
 }
