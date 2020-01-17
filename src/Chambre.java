968b32bef7f299c48484ff4000c0807c3e112c3c
AdriMer <adrien.mercelat@orange.fr>
2020-01-17 14:23:29 +0100

    Chambre class

--- src/Chambre.java
+++ src/Chambre.java
@@ -1,20 +1,45 @@
 public class Chambre {
     private int numero;
     private int capacite;
+    private float prix;
+    private Categorie maCategorie;
+
+    public Chambre(int numero, int capacite, float prix, Categorie maCategorie) {
+        this.numero = numero;
+        this.capacite = capacite;
+        this.prix = prix;
+        this.maCategorie = maCategorie;
+    }
 
     public int getNumero() {
         return numero;
-    }
-
-    public int getCapacite() {
-        return capacite;
     }
 
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
+    public int getCapacite() {
+        return capacite;
+    }
+
     public void setCapacite(int capacite) {
         this.capacite = capacite;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public Categorie getMaCategorie() {
+        return maCategorie;
+    }
+
+    public void setMaCategorie(Categorie maCategorie) {
+        this.maCategorie = maCategorie;
     }
 }
